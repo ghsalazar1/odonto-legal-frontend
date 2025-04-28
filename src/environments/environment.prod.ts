75e1bb7522ae8b0a97b7f0f6dbaf707ee862f491
Gleybe Hebson Salazar Cavalcanti <gleybe.cavalcanti@vectracs.com.br>
2025-04-28 01:41:10 -0300

    subindo readme.md

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,6 @@
 export class Environment {
     public static isProduction = true;
     public static BackendURL = 'https://odonto-legal-backend-x0x2.onrender.com';
-    public static FrontendURL = 'https://odonto-legal.netlify.app'
-    public static EnvironmentName = 'production';
     
 }
   
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export class Environment {
     public static isProduction = false;
     public static BackendURL = 'http://localhost:3000';
-    public static EnvironmentName = 'development';
 }
   
